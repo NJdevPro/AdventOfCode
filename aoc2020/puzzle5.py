292aed9e6c9f6a299b9130ba76e4019ce70eed5b
NJdevPro <30365898+NJdevPro@users.noreply.github.com>
2020-12-06 20:00:55 +0100

    Update puzzle5.py

--- aoc2020/puzzle5.py
+++ aoc2020/puzzle5.py
@@ -4,26 +4,25 @@ print("-----------Part 1-----------")
 value = {'F': '0', 'B': '1', 'L': '0', 'R': '1'}
 
 def rownum(row):
-    binary_form = [value[data] for data in row]
-    return int(''.join(binary_form), 2)
+    binary = [value[data] for data in row]
+    return int(''.join(binary), 2)
 
 def colnum(col):
-    binary_form = [value[data] for data in col]
-    return int(''.join(binary_form), 2)
+    binary = [value[data] for data in col]
+    return int(''.join(binary), 2)
 
-maxID = 0
+maxseat = 0
 seats = []
 for bpass in boarding_passes:
     seat = rownum(bpass[:7]) * 8 + colnum(bpass[7:])
     seats.append(seat)
-    maxID = max(seat, maxID)
+    maxseat = max(seat, maxseat)
 
-print(maxID)
+print(maxseat)
 
 print("-----------Part 2-----------")
 seats.sort()
 i = 1
-
 while i < len(seats):
     if seats[i] != seats[i - 1] + 1:
         print(seats[i] - 1)
