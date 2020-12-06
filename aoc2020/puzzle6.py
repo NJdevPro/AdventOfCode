ff5275739051b3c20efab44aea60f94619c8b448
NJdevPro <30365898+NJdevPro@users.noreply.github.com>
2020-12-06 22:04:01 +0100

    Revert previous update

--- aoc2020/puzzle6.py
+++ aoc2020/puzzle6.py
@@ -1,4 +1,5 @@
 import itertools
+from collections import Counter
 
 print("-----------Part 1-----------")
 groups, group  = [], []
@@ -10,13 +11,10 @@ for line in open("puzzle6.txt","r"):
         group.append(line.strip())
 groups.append(group)
 
-def make_set(group):
+def count_group(group):
     words = [list(word) for word in group]
     letters = list(itertools.chain(*words))
-    return set(letters)
-
-def count_group(group):
-    return sum([len(make_set(group))])
+    return sum([len(set(letters))])
 
 print(sum([count_group(g) for g in groups]))
 
@@ -24,7 +22,9 @@ print("-----------Part 2-----------")
 
 def count2_group(group):
     l = len(group)
-    count = make_set(group)
+    words = [list(word) for word in group]
+    letters = list(itertools.chain(*words))
+    count = Counter(letters)
     return sum([1 for x in count.values() if x == l])
 
 print(sum([count2_group(g) for g in groups]))
