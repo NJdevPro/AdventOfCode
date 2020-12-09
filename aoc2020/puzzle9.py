44ad6842e72d66a26527f600530321f52755dfd2
NJ dev <30365898+NJdevPro@users.noreply.github.com>
2020-12-09 08:34:17 +0100

    puzzle 9

---
+++ aoc2020/puzzle9.py
@@ -0,0 +1,28 @@
+numbers = [int(n) for n in open("puzzle9.txt", 'r').read().splitlines()]
+
+def find_sum(n):
+    for n1 in range(n-25,n):
+        for n2 in range(n-25,n):
+            if n1 == n2:
+                continue
+            if numbers[n1] + numbers[n2] == numbers[n]:
+                return n+1, numbers[n]
+    print(n+1, numbers[n])
+    return n+1, numbers[n]
+
+def find_sum2(sum1, n):
+    #print(sum1, n)
+    for n1 in range(n):
+        for n2 in range(n1+1,n,1):
+            s = sum(numbers[n1:n2])
+            if s > sum1:
+                break
+            if s == sum1:
+                print(s, n1+1, n2+1, min(numbers[n1:n2])+max(numbers[n1:n2]), numbers[n1:n2])
+
+for n in range(25, len(numbers)):
+    idx, ss = find_sum(n)
+
+find_sum2(85848519, 554)
+
+
