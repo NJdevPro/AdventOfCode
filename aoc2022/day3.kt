4fadf13d585a3d7e17d929150f167f6f06c5b337
Nicolas Janin <30365898+NJdevPro@users.noreply.github.com>
2022-12-03 09:43:23 +0100

    replace var with val

--- aoc2022/day3.kt
+++ aoc2022/day3.kt
@@ -8,7 +8,7 @@ fun line2LetterVal(line: String): Int{
     val half1 = line.toList().subList(0, line.length/2+1)
     val half2 = line.toList().subList(line.length/2, line.length)
     for (c in half1) {
-        var found = half2.filter{it == c};
+        val found = half2.filter{it == c};
         if (found.isNotEmpty()) {return letter2Val(c) }
     }
     return 0
@@ -17,7 +17,7 @@ fun line2LetterVal(line: String): Int{
 fun part1(lines: List<String>):Int = lines.map { line2LetterVal(it) }.sum()
 
 fun lines2Val(group:List<String>): Int {
-    var found = group[0].toList().intersect(group[1].toList().intersect(group[2].toList())).last()
+    val found = group[0].toList().intersect(group[1].toList().intersect(group[2].toList())).last()
     return letter2Val(found)
 }
 
