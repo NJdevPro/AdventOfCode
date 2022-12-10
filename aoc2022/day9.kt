29f2529135975b0d22fdb48e6608dc8f594a7334
Nicolas Janin <30365898+NJdevPro@users.noreply.github.com>
2022-12-10 20:50:14 +0100

    Solve part 2

--- aoc2022/day9.kt
+++ aoc2022/day9.kt
@@ -2,13 +2,12 @@ package AOC2022
 
 import java.io.File
 import java.lang.Integer.max
+import kotlin.math.abs
 import kotlin.math.min
-
-typealias Pos = Day9.Knot
 
 class Day9 {
 
-    data class Knot (var x: Int, var y: Int) {
+    data class Pos (var x: Int, var y: Int) {
         operator fun plus(pos: Pos?) : Pos = Pos(this.x + pos!!.x, this.y + pos.y)
         operator fun minus(pos : Pos) : Pos = Pos(this.x - pos.x, this.y - pos.y)
         companion object {
@@ -16,8 +15,8 @@ class Day9 {
         }
     }
 
-    var H = Knot(0,0)
-    var T = Knot(0, 0)
+    var H = Pos(0,0)
+    var T = Pos(0, 0)
 
     fun draw() {
         val xmin = node.map{it.x}.min() - 4
@@ -38,74 +37,67 @@ class Day9 {
                 println()
             }
             println()
+    }
 
+    fun follow (H: Pos, T: Pos) : Pos {
+        val dist = H - T
+        var T2 = T
+        if (abs(dist.x) > 1 && abs(dist.y) > 1) {
+            T2 = Pos(if (H.x > T.x) H.x - 1 else H.x + 1, if (H.y > T.y) H.y - 1 else H.y + 1)
+        } else if (abs(dist.x) > 1) {
+            T2 = Pos(if (H.x > T.x) H.x - 1 else H.x + 1, H.y)
+        } else if (abs(dist.y) > 1) {
+            T2 = Pos(H.x, if (H.y > T.y) H.y - 1 else H.y + 1)
+        }
+        return T2
     }
 
     fun moveString(move: String) : Set<Pos> {
         var visited : MutableSet<Pos> = mutableSetOf()
         move.split(' ').let { (dir, steps) ->
             for(step in 1..steps.toInt()) {
-                H += Knot.move[dir]    // move head
-                val dist = H - T
-                if (H.x != T.x && H.y != T.y){
-                    when (dist){
-                        Pos(2,1), Pos(1,2) -> {T += Pos(1,1)}
-                        Pos(2,-1), Pos(1, -2) -> {T += Pos(1,-1)}
-                        Pos(-2,1), Pos(-1, 2) -> {T += Pos(-1,1)}
-                        Pos(-2,-1), Pos(-1, -2) ->{T += Pos(-1,-1)}
-                    }
-                } else {
-                    if (dist.x > 1) T.x++ else if (dist.x < -1) T.x--
-                    if (dist.y > 1) T.y++ else if (dist.y < -1) T.y--
-                }
+                H += Pos.move[dir]    // move head
+                T = follow(H, T)
                 visited.add(T.copy())
-                println("move  ${step}, ${dir}, H=${H}, T=${T}")
-                draw()
             }
         }
         return visited
     }
 
-    var node: Array<Knot> = Array(size=10, init = { Knot(0,0)})
+    var node: Array<Pos> = Array(size=10, init = { Pos(0,0)})
 
     fun moveRope(move: String, n:Int) : Set<Pos> {
         var visited : MutableSet<Pos> = mutableSetOf()
         move.split(' ').let { (dir, steps) ->
             for(step in 1..steps.toInt()) {
-                node[0] = node[0] + Knot.move[dir]    // move head
-                for (i in 1..n-2) {
-                    H = node[i]
-                    T = node[i+1]
-                    val dist = H - T
-                    if (H.x != T.x && H.y != T.y) {
-                        when (dist) {
-                            Pos(2, 1), Pos(1, 2) -> T += Pos(1, 1)
-                            Pos(2, -1), Pos(1, -2) -> T += Pos(1, -1)
-                            Pos(-2, 1), Pos(-1, 2) -> T += Pos(-1, 1)
-                            Pos(-2, -1), Pos(-1, -2) -> T += Pos(-1, -1)
-                        }
-                    } else {
-                        if (dist.x > 1) T.x++ else if (dist.x < -1) T.x--
-                        if (dist.y > 1) T.y++ else if (dist.y < -1) T.y--
-                    }
+                node[0] = node[0] + Pos.move[dir]    // move head, compiler bug against +=
+                H = node[0]
+                for (i in 0..n-2) {
+                    node[i+1] = follow(node[i], node[i+1])
                 }
+                T = node.last()
                 visited.add(T.copy())
-                println("move  ${step}, ${dir}, H=${H}, T=${T}")
-                draw()
+                //println("move  ${step}, ${dir}, H=${H}, T=${T}")
             }
         }
         return visited
     }
 
     fun part1(lines: List<String>) : Int {
-        var positions : MutableSet<Knot> = mutableSetOf()
-        lines.forEachIndexed{ step, move -> println("Step" + step + " - "+ move); positions.addAll(moveString(move));  }
+        var positions : MutableSet<Pos> = mutableSetOf()
+        lines.forEachIndexed{ step, move ->
+            //println("Step" + step + " - "+ move);
+            positions.addAll(moveString(move));
+        }
         return positions.size
     }
 
     fun part2(lines: List<String>) : Int {
-        var positions : MutableSet<Knot> = mutableSetOf()
-        lines.forEachIndexed{ step, move -> println("Step" + step + " - "+ move); positions.addAll(moveRope(move, 10));  }
+        var positions : MutableSet<Pos> = mutableSetOf()
+        lines.forEachIndexed{ step, move ->
+            //println("Step" + step + " - "+ move);
+            positions.addAll(moveRope(move, 10));
+        }
         return positions.size
     }
 
